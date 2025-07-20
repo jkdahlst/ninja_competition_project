0e09027f0077f936395ca8109d5ba34f846aa371
Jason Dahlstrom <jkdahlst@gmail.com>
2025-07-19 22:06:37 -0500

    Change calendar layout to start on monday for each week

--- components/CalendarView.tsx
+++ components/CalendarView.tsx
@@ -172,6 +172,7 @@ export default function CalendarView({ competitions }: CalendarViewProps) {
           initialView="dayGridMonth"
           dayMaxEventRows={true}
           dayMaxEvents={4}
+          firstDay={1} // Start week on Monday
           height="auto"
           eventContent={(arg) => (
             <div className="text-xs px-1 py-0.5 leading-snug whitespace-normal break-words">
